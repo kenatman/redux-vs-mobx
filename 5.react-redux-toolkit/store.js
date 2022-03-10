6bef568e3ce2c9d7a50365d822905a755f6dff42
Yunsu <kenatman0723@gmail.com>
2022-03-10 16:13:18 +0900

    리덕스툴킷 스토어 코드 간결하게 변경

---
+++ 5.react-redux-toolkit/store.js
@@ -0,0 +1,17 @@
+const {configureStore, getDefaultMiddleware} = require('@reduxjs/toolkit')
+const reducer = require('./reducers');
+
+
+const firstMiddleware = (store) => (next) => (action) => {
+    console.log('액션로깅!@@', action);
+    next(action);
+};
+
+
+const store = configureStore({
+    reducer,
+    middleware: [firstMiddleware, ...getDefaultMiddleware()],
+    devTools: process.env.NODE_ENV !== 'production',
+});
+
+module.exports = store;
