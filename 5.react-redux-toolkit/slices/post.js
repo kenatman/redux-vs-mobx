eae4652809b2469ff3829315b0a4fedadb2aab7a
Yunsu <kenatman0723@gmail.com>
2022-03-13 11:52:04 +0900

    extraReducers builder 사용+ 공통처리

--- 5.react-redux-toolkit/slices/post.js
+++ 5.react-redux-toolkit/slices/post.js
@@ -3,27 +3,33 @@ const {addPost} = require('../thunks/post');
 
 
 const initialState = {
-    list: [],
-    isLoading: false,
+    list: [], isLoading: false,
 };
 
 const postSlice = createSlice({
     name: 'post', initialState,
     reducers: {},
-    extraReducers: {
-        [addPost.pending]: (state, action) => {
+    extraReducers: (builder) => builder
+       /* .addCase(addPost.pending, (state, action) => {
             state.isLoading = true;
-        },
-        [addPost.fulfilled]: (state, action) => {
+        })*/
+        .addCase(addPost.fulfilled, (state, action) => {
             state.isLoading = false;
             state.list.push(action.payload);
             return state;  // immer 사용시 state를 직접 바꾸는 경우 가끔 불변성이 깨지는데 return state; 해주면 해결.
-        },
-        [addPost.rejected]: (state, action) => {
+        })
+        .addCase(addPost.rejected, (state, action) => {
             state.isLoading = false;
-        }
-    }
-
+        })
+        // 공통처리
+        .addMatcher((action) => {
+            return action.type.includes('/pending');
+        }, (state, action) => {
+            state.isLoading = true;
+        })
+        .addDefaultCase((state, action) => {
+            // default
+        })
 })
 
 
