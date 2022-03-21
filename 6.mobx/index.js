4e8a9e165f910e6c05810933b9e1d6b4ee7037aa
Yunsu <kenatman0723@gmail.com>
2022-03-21 10:52:09 +0900

    mobx : reaction, action

--- 6.mobx/index.js
+++ 6.mobx/index.js
@@ -1,4 +1,4 @@
-const {observable, autorun, runInAction} = require('mobx');
+const {observable, autorun, runInAction, reaction, action} = require('mobx');
 
 const state = observable({
     compA: 'a',
@@ -6,9 +6,26 @@ const state = observable({
     compC: null
 });
 
+// 다 감지한다.
 autorun(()=>{
     console.log('changed', state.compA)
-})
+});
+
+// 특정 값이 변할 때만 감지할 수 있다.
+reaction(()=>{
+ return state.compB
+}, ()=>{
+    console.log('reaction', state.compB)
+});
+
+// 바로 실행하지 않을 때.. action(()=>{})() === runInAction
+const change = action(()=>{
+    state.compA = 'f';
+    state.compB = 56;
+    state.compC = 'hehe';
+});
+
+change();
 
 runInAction(()=>{
     state.compA = 'c';
@@ -18,4 +35,5 @@ runInAction(()=>{
 
 runInAction(()=>{
     state.compA = 'd';
-})
+});
+
