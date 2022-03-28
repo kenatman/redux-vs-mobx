163d23a3355c2ca4ed96126028bf2f73e6a26763
Yunsu <kenatman0723@gmail.com>
2022-03-28 14:14:18 +0900

    fork(안기다림,비동기) & call(기다림,동기)

--- 9.redux-saga/sagas/user.js
+++ 9.redux-saga/sagas/user.js
@@ -6,9 +6,12 @@ const HELLO_SAGA = 'HELLO_SAGA';
 function loginAPI() {
     // 서버에 요청
 }
-
+// call, fork 둘다 함수를 호출하는 것.
+// call은 순서가 중요할 때 사용. 끝날 때까지 기다림.
+// fork는 순서 상관 없을 때. 요청하고 다음것 실행.
 function* login() {
     try {
+        yield fork(logger); // logger는 내 기록을 로깅하는 함수, 10초 걸림.
         yield call(loginAPI);
         yield put({ // put은 dispatch와 동일
             type: LOG_IN_SUCCESS
@@ -23,26 +26,24 @@ function* login() {
 
 function* watchLogin() {
     // takeLatest는 LOG_IN 액션이 dispatch 되길 기다렸다가 login 제너레이터를 호출한다.
-    yield take(LOG_IN_REQUEST);
-    yield delay(2000);
-    yield put({
-        type: LOG_IN_SUCCESS
-    })
+    yield takeEvery(LOG_IN_REQUEST, login);
 }
 
-function* watchSignUp(){
+function* watchSignUp() {
 
+}
+
+function* hello() {
+    yield delay(1000);
+    yield put({
+        type: 'BYE_SAGA'
+    });
 }
 
 // takeEvery는 저네러이터 + while(true) 역할
 // takeLatest는 이전요청이 끝나지 않은 게 있다면 이전 요청을 취소함!! => ex) 로그인 여러번 요청해도 마지막 한번만 처리.
-function* watchHello(){
-    yield takeLatest(HELLO_SAGA, function*(){
-        yield delay(1000);
-        yield put({
-            type: 'BYE_SAGA'
-        })
-    })
+function* watchHello() {
+    yield takeLatest(HELLO_SAGA, hello)
 }
 
 
@@ -59,8 +60,8 @@ function* watchHello(){
 
 export default function* userSaga() {
     yield all([
-        watchLogin(),
-        watchSignUp(),
-        watchHello(),
+        fork(watchLogin),
+        fork(watchSignUp),
+        fork(watchHello),
     ])
 }
